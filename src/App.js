21787b5fcb9565d13fc170f9c20ac96f192f3896
OhmKung <henryohohm@gmail.com>
2021-02-26 22:31:07 +0700

    navbar
    testnavbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Navbar from './Navbar'
+
 
 function App() {
   return (
-    <div className="App">
-      Hi
+    <div class="container-fluid">
+      <Navbarx />
     </div>
-  );
+  )
 }
 
 export default App;
+
+function Navbarx() {
+  return(
+    <div>
+      <Navbar />
+    </div>
+  )
+}
